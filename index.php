f6352667a039e66dbf89f4c1d8c43b6ed4776063
JakobJR <jakobjr@hotmail.com>
2020-11-02 21:45:58 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <a href="#">
+                <img src="img/logo.png" alt="">
+            </a>
+        </div>
+        <div class="menu-items">
+            <ul>
+                <li class="hover hover-1"><a href="#">Home</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="nav-placeholder"></div>
+
+    <div class="hero-wrapper">
+        <div class="shadow-overlay"></div>
+        <div class="hero-text">
+            <h4>Napples<br>& Chill</h1>
+            <h5>- Order for a night in</h2>
+        </div>
+    </div>
+
+    <div class="section-1">
+        <h2>Best pizza in town</h2>
+    </div>
+    
+</body>
+</html>
